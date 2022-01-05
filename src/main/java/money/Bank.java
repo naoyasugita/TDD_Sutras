16910efff2361a4a8e6aae403101fd291be3d58b
naoyasugita <sugita6878@gmail.com>
2022-01-05 17:56:50 +0900

    p93,94 リファクタリング

--- src/main/java/money/Bank.java
+++ src/main/java/money/Bank.java
@@ -3,8 +3,7 @@ package money;
 public class Bank {
   Money reduce(Expression source, String to){
     Sum sum = (Sum) source;
-    int amount = sum.augend.amount + sum.addend.amount;
-    return new Money(amount, to);
+    return sum.reduce(to);
   }
 
 }
