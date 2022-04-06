e88a41c7efb0f5338309f1e96c1e03669c53e1ea
naoyasugita <sugita6878@gmail.com>
2022-04-06 17:23:29 +0900

    P125 Money.timesの実装

--- src/main/java/money/Sum.java
+++ src/main/java/money/Sum.java
@@ -10,6 +10,11 @@ public class Sum implements Expression{
   }
 
   @Override
+  public Expression times(int multiplier) {
+    return new Sum(augend.times(multiplier), addend.times(multiplier));
+  }
+
+  @Override
   public Expression plus(Expression addend) {
     return new Sum(this, addend);
   }
--- src/test/java/money/MoneyTest.java
+++ src/test/java/money/MoneyTest.java
@@ -90,6 +90,17 @@ public class MoneyTest {
     bank.addRate("CHF", "USD", 2);
     Money result = bank.reduce(fiveBucks.plus(tenFrancs), "USD");
     assertEquals(Money.dollar(10), result);
-
   }
+
+  @Test
+  public void  testSumTimes() {
+    Expression fiveBucks = Money.dollar(5);
+    Expression tenFrancs = Money.franc(10);
+    Bank bank = new Bank();
+    bank.addRate("CHF", "USD", 2);
+    Expression sum = new Sum(fiveBucks, tenFrancs).times(2);
+    Money result = bank.reduce(sum, "USD");
+    assertEquals(Money.dollar(20), result);
+  }
+
 }
